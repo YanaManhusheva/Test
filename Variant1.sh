c445d612ea658b8a0b62645c941b099cdb75c07d
Yana Manhusheva <yanamanhusheva@gmail.com>
2020-12-23 02:14:41 +0200

    Added bash script file

---
+++ Variant1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Type one name of the following txt files: "
+for var in *.txt
+do
+echo "${var%.*}"
+done
+read name
+
+echo "Type the word you would like to find : "
+read word
+
+grep -e $word $name.txt
